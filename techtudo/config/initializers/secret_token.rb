84da17a96acd678922341325e7262d1a1ee1b2a9
Guilherme Cirne <gcirne@gmail.com>
2013-01-07 14:17:14 -0200

    techtudo inicial.

---
+++ techtudo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techtudo::Application.config.secret_token = '482c283a5d059252ebe7fc467ee77f4a3e4c7d486d4cd8f076bb5f346ff751141e531755bd706e22da352ec5520f0d99c6a73ee186bae2d58492665d8589b9b0'
---
+++ techtudo/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Techtudo::Application.config.session_store :cookie_store, key: '_techtudo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Techtudo::Application.config.session_store :active_record_store
