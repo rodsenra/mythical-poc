eca0ec41c04791eca19fbeec18b9ac7ba27b5984
Claudio Figueiredo <jcfigueiredo@gmail.com>
2013-01-07 18:04:59 -0200

    cma form app

---
+++ jsonform/playground/js/cma.js
@@ -0,0 +1,28 @@
+$('document').ready(function () {
+    $.ajax({
+        url: 'http://10.2.180.22:5100/schemas/1',
+        dataType: 'json'
+    }).done(function (code) {
+        $.extend(code, 
+            {onSubmit: function (errors, values) {
+                
+                var action = $('#form').attr('action');
+
+                jQuery.support.cors = true;
+                $.ajax({
+                  type: 'POST',
+                  url: action,
+                  data: JSON.stringify(values),
+                    contentType: 'application/json; charset=UTF-8',
+                    dataType: 'json'
+                });
+                
+                return false;
+        }});
+
+        $('#form').jsonForm(code);
+        
+    }).fail(function (code) {
+        $('#result').html('Sorry, I could not retrieve the example!');
+    });
+});
