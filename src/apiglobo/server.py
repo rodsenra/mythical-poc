6114e6e5bfbedb2dcf99c39545e41c3b9d45f4ec
rodsenra <rodsenra@gmail.com>
2013-01-14 18:29:30 -0200

    Adds healthcheck route

--- src/apiglobo/server.py
+++ src/apiglobo/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import sys
 # DISCLAIMER: Hack Maligno in POC: xGH - viabilizar override da config em diferentes ambientes
 from apiglobo import middleware, settings
@@ -24,6 +24,9 @@ app.config.from_pyfile("settings.py")
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.route("/healthcheck")
+def healthcheck():
+    return "ok"
 
 app.register_blueprint(data.data_blueprint)
 app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
