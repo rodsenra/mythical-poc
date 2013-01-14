95777f0cfd162165111cf5b2d512fa9a6bfd9bfb
Guilherme Cirne <gcirne@gmail.com>
2013-01-14 18:26:56 -0200

    remove gambiarra de settings pra rodar com gunicorn.

--- src/apiglobo/server.py
+++ src/apiglobo/server.py
@@ -4,26 +4,17 @@
 from flask import Flask, render_template
 import sys
 # DISCLAIMER: Hack Maligno in POC: xGH - viabilizar override da config em diferentes ambientes
-from apiglobo import middleware, settings
-
-
-if len(sys.argv) > 1:
-    override_settings = __import__(sys.argv[1])
-    settings.__dict__.update(override_settings.__dict__)
-
-
-from apiglobo import data
+from apiglobo import data, middleware, settings
 
 
 app = Flask(__name__)
-app.wsgi_app = middleware.LogMiddleware(app)
+# app.wsgi_app = middleware.LogMiddleware(app)
 app.config.from_pyfile("settings.py")
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
-app.register_blueprint(data.data_blueprint)
-app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
+if __name__ == '__main__':
+    # app.register_blueprint(data.data_blueprint)
+    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
